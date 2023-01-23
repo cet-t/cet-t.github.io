f31ce2f3ca01efdf6c3fa1ee3884b24fb419ac0d
cet <67916285+cet-t@users.noreply.github.com>
2023-01-23 08:18:02 +0000

    f

---
+++ check.js
@@ -0,0 +1,8 @@
+function check(){
+    var fileList = document.getElementById("sFiles").files;
+    var list = "";
+    for(var i=0; i<fileList.length; i++){
+    list += "[" + fileList[i].size + " bytes]" + fileList[i].name + "<br>";
+    }
+    document.getElementById("result").innerHTML = list;
+}
