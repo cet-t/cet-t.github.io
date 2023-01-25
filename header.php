ff6b3049b7f1da4d1d6c72d93266629d98fc3d1c
cet021 <cets021@gmail.com>
2023-01-25 20:00:43 +0900

    f

---
+++ header.php
@@ -0,0 +1,10 @@
+<div class="header">
+    <nav>
+      <ul>
+        <li><a href="<?php print $path; ?>index.html">TOP</a></li>
+        <li><a href="<?php print $path; ?>arcade.html">ARCADE</a></li>
+        <li><a href="<?php print $path; ?>download.html">REPOSITORY</a></li>
+        <li><a href="<?php print $path; ?>sprites.html">SPRITES</a></li>
+      </ul>
+    </nav>
+  </div>
